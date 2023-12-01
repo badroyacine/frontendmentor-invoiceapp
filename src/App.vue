bf39bedcfcdb029859347f8511efc905fec147bc
badroyacine <badreddine.yacine99@gmail.com>
2023-12-01 14:53:05 +0100

    scss main structure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="bg-primary fs-2xl fs-md-xl">
+    <h1 class="mt-0">Home page App</h1>
+  </div>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/scss/abstracts/functions.scss" as *;
+          @use "@/assets/scss/abstracts/mixins.scss" as *;
+          @use "@/assets/scss/base/variables.scss" as *;
+        `,
+      }
+    },
+  },
   plugins: [
     vue(),
   ],
