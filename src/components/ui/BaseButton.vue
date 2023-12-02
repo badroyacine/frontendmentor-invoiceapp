e576594fbf662ef0d5e1c87b8b07be963530e5e7
badroyacine <badreddine.yacine99@gmail.com>
2023-12-02 15:43:39 +0100

    BaseButton & BaseDialog components

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,115 @@
+<template>
+    <component
+        :is="componentName"
+        :class="componentClasses" 
+        :disabled="disabled"
+        v-bind="$attrs"
+    >
+        <div class="btn__content">
+            <div v-if="iconSrc" class="icon-container">
+                <img :src="iconSrc">
+            </div>
+            <span class="btn__label fw-bold">{{ label }}</span>
+        </div>
+    </component>
+</template>
+
+<script setup>
+import { computed } from "vue"
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: true
+    },
+    iconSrc: {
+        type: String,
+        default: ''
+    },
+    color: {
+        type: String,
+        default: 'primary'
+    },
+    routerLink: {
+        type: Boolean,
+        default: false
+    },
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const componentName = computed(() => props.routerLink ? 'router-link' : 'button')
+const componentClasses = computed(() => ([
+    'btn', 
+    props.color, 
+    { 'icon-btn': !!props.iconSrc },
+]))
+
+</script>
+
+<style lang="scss" scoped>
+.btn {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    border-radius: 24px;
+    padding: 1.5em;
+    border: none;
+    height: 48px;
+    cursor: pointer;
+    transition: all .2s ease;
+    text-decoration: none;
+
+    &__content {
+        @include flex(center, flex-start, 16px);
+    }
+
+    &__label {
+        letter-spacing: -0.25px;
+    }
+}
+
+.icon-btn {
+    padding: 0.5em 1.125em 0.5em 0.5em;
+}
+
+.icon-container {
+    @include flex-center;
+    width: 32px;
+    height: 32px;
+    background-color: #fff;
+    border-radius: 50%;
+}
+
+.primary {
+    background-color: $primary-color;
+    &:hover {
+        background-color: $primary-light-color;
+    }
+}
+
+.danger {
+    background-color: $danger-color;
+    &:hover {
+        background-color: #FF9797;
+    }
+}
+
+.secondary-light {
+    background-color: #F9FAFE;
+    color: $secondary-light-color;
+    &:hover {
+        background-color: $light-color;
+    }
+}
+
+.secondary-dark {
+    background-color: $secondary-dark-color;
+    color: $secondary-color;
+    &:hover {
+        background-color: #0C0E16;
+    }
+}
+</style>
---
+++ src/components/ui/BaseDialog.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="overlay">
+        <div class="dialog">
+            <h3 class="dialog__title">{{ title }}</h3>
+            <p class="dialog__content mt-12 mb-14">{{ content }}</p>
+            <div class="dialog__actions">
+                <BaseButton
+                    label="Cancel"
+                    color="secondary-light"
+                    @click="closeDialog"
+                    :disabled="actionsDisabled"
+                ></BaseButton>
+                <BaseButton 
+                    :color="actionBtnColor"
+                    :label="actionBtnText"
+                    @click="actionClick"
+                    :disabled="actionsDisabled"
+                ></BaseButton>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import BaseButton from './BaseButton.vue';
+
+const emit = defineEmits(['closeDialog', 'actionClick'])
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    actionBtnText: {
+        type: String,
+        required: true
+    },
+    actionBtnColor: {
+        type: String,
+        default: 'primary'
+    },
+    actionsDisabled: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const closeDialog = () => emit('closeDialog')
+const actionClick = () => emit('actionClick')
+
+</script>
+
+<style lang="scss" scoped>
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    background-color: rgba(#000000, 0.5);
+    width: 100vw;
+    height: 100vh;
+}
+
+.dialog {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: $list-item-bg-color;
+    width: 90%;
+    max-width: 480px;
+    border-radius: 8px;
+    padding: 3em;
+
+    &__title {
+        @include custom-text(fs(xl), $main-color, 32px, -0.5px, 700);
+    }
+
+    &__content {
+        @include custom-text(fs(sm), $main-text-color, 22px, -0.1px);
+    }
+
+    &__actions {
+        @include flex(center, flex-end, 8px);
+    }
+}
+</style>
